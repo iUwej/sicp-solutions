6ff960d62ac1524b0ff026ef99a3378e2cd2e0a1
iUwej <wawerumunene@gmail.com>
2018-09-22 19:18:26 +0300

    added more exercises

--- objects.scm
+++ objects.scm
@@ -32,7 +32,7 @@
 		)
 	dispatch
 	)
-
+;Ex 3.2
  (define (make-monitored f)
  	(define (mf value)
  		(lambda (arg)
@@ -42,5 +42,86 @@
  				)
  			)
  	)
- 	(mf 0)
- 	)
+ 	(mf 0))
+
+
+					; Ex 3.3
+
+	(define (make-protected-account balance secret-password)
+
+	  (define (withdraw amount)
+	    (if (>= balance amount)
+		(begin (set! balance (- balance amount)) balance)
+		"Insufficient fund")
+	    )
+	  (define (deposit amount)
+	    (set! balance(+ balance amount))
+	    balance
+	    )
+	  (define (dispatch password m)
+	    (if (eq? secret-password password)
+		(cond
+		 ((eq? m 'withdraw) withdraw)
+		 ((eq? m 'deposit) deposit)
+		 (else (error "Uknown request -- MAKE-PROTECTED-ACCOUNT" m)))
+		(error "Incorrect-password"))
+	    )
+	  dispatch)
+
+					; Ex 3.4
+
+
+(define (make-vaulted-account balance secret-password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount)) balance)
+	"Insufficient fund")
+    )
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance
+    )
+
+  (define (call-the-cops args) "Calling the cops, on you")
+
+  (define (make-vaulted count )
+
+    (lambda (password m)
+      (if (eq? secret-password password)
+	  (begin (set! count 0)
+		 (cond
+		  ((eq? m 'withdraw) withdraw)
+		  ((eq? m 'deposit) deposit)
+		  (else (error "Uknown request -- MAKE-PROTECTED-ACCOUNT" m))))
+	  (begin (set! count (+ 1 count))  (if(>= count 7) call-the-cops  (error "Incorrect-password")))
+	  ) )
+    )
+  (make-vaulted 0))
+
+
+					; Ex 3.5
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+	   (/ trials-passed trials)
+	   )
+	  ((experiment)
+	   (iter (- trials-remaining 1) (+ trials-passed 1)))
+	  (else (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (estimate-integral P x1 x2 y1 y2 trials)
+
+  (define  (experiment)
+    (P (random-in-range x1  x2) (random-in-range y1 y2)))
+
+  (monte-carlo trials experiment))
+
+
+	   
+  
